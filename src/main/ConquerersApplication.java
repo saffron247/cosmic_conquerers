5a256df5ac60ec2bee273d1206a84b94c5702f16
saffron247 <ethanbaldwin247@gmail.com>
2024-04-08 10:57:10 -0400

    fixed multimedia2.jar

---
+++ src/main/ConquerersApplication.java
@@ -0,0 +1,30 @@
+package main;
+
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+import app.JApplication;
+
+public class ConquerersApplication extends JApplication implements ActionListener
+{
+  public ConquerersApplication(int width, int height)
+  {
+    super(width, height);
+    // TODO Auto-generated constructor stub
+  }
+  public static final int WIDTH = 600;
+  public static final int HEIGHT = 800;
+  
+  @Override
+  public void actionPerformed(ActionEvent e)
+  {
+    // TODO Auto-generated method stub
+    
+  }
+  @Override
+  public void init()
+  {
+    // TODO Auto-generated method stub
+    
+  }
+}
